f9ff9b43d0a9aedfd1e01b77bbc1ba0e227b0286
fawwaz <88119082+fawwazalifiofarsa@users.noreply.github.com>
2022-03-15 12:07:05 +0700

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -15,7 +15,8 @@ class Home extends React.Component {
             <div className="w-full mt-10 px-40">
                 <div className="bg-white shadow-md rounded-lg px-8 pt-8 pb-8 mb-5">
                     <h1 className="text-center mb-5 text-2xl font-semibold">Selamat Datang !</h1>
-                    <div className="mb-2 text-center justify-center  text-black text-lg font-semibold opacity-80 hover:opacity-100 duration-300"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" className='mx-auto mb-2 w-1/4 cursor-pointer' onClick={this.linkGithub}/>
+                    <div className="mb-2 text-center justify-center  text-black text-lg font-semibold opacity-80 hover:opacity-100 duration-300">
+                        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" className='mx-auto mb-2 w-1/4 cursor-pointer' onClick={this.linkGithub}/>
                         <p className='mx-auto'>GitHub</p>
                     </div>
                 </div>
